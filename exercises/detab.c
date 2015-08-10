05f9abb6e096c36019ee459872b4d0319f833421
Luca Bertoni <luca.bertoni24@gmail.com>
2015-08-10 13:14:25 +0200

    Fixed detab.c

--- exercises/detab.c
+++ exercises/detab.c
@@ -1,82 +1,73 @@
-/**/
+/*
+	Cosa fa			:			Sostituisce i tab presenti in una stringa presa da input
+								con un numero definito di spazi
+*/
 
-#include <stdio.h>
+#include <stdio.h>	/* Libreria per lo std input e output */
 
-#define SPACETAB		4
-#define MAXBUFFERLEN	10000
+#define SPACETAB		4	/* Numero di spazi con cui sostituire i tab */
+#define MAXBUFFERLEN	1000	/* Massima lunghezza che può assumere la stringa */
 
-void makeSpaces(char sSpaces[],int nSpaces);
-void copy(char sFrom[],char sTo[]);	/* Vedi descrizione sotto */
-void replaceChrWithString(char sBuffer[],char sRet[],int cCarattere,char sReplace[],int nStart,int nTimes);
+void makeSpaces(char sSpaces[],int nSpaces); /* Vedi la descrizione sotto */
+void append(char sWhat[], char sTo[]);	/* Vedi la descrizione sotto */
 
 main(){
-	int cCarattere,nCount,nSpaceCount,nPosition;
-	char sBuffer[MAXBUFFERLEN];
-	char sTemp[MAXBUFFERLEN];
-	char sSpaces[SPACETAB];
-	nCount = nSpaceCount = 0;
+	char cCarattere[1];	/* 	Inizializzo un array di caratteri di un solo elemento, questo perhcè la funzione
+							append si aspetta sempre un array di caratteri 
+						*/
+	char sBuffer[MAXBUFFERLEN];	/* Definisco la variabile che conterrà l'intero buffer */
+	char sSpaces[SPACETAB];	/* Definisco la variabile che conterrà il numero variabile di spazi */
 
-	makeSpaces(sSpaces,SPACETAB);
+	makeSpaces(sSpaces,SPACETAB); /* Creo l'array di spazi */
 
-	while((cCarattere=getchar()) != EOF){
-		if (nCount == MAXBUFFERLEN-1)
-			break;
+	/* Ciclo fino a quando il carattere inserito non è uno EOF */
+	while((cCarattere[0]=getchar()) != EOF){
 
-		sBuffer[nCount] = cCarattere;
-
-		if (cCarattere == '\t'){
-			replaceChrWithString(sBuffer,sTemp,cCarattere,sSpaces,nCount,1);
-			copy(sTemp,sBuffer);
+		/*	Controllo che carattere è stato inserito
+			Se è un tab, al buffer aggiungo gli spazi,
+			altrimenti aggiungo il carattere inserito
+		*/
+		if (cCarattere[0] == '\t'){
+			append(sSpaces,sBuffer);
+		}else{
+			append(cCarattere,sBuffer);
 		}
 	}
+
+	/* Stampo il buffer generato */
 	printf("\nsBuffer:%s\n", sBuffer);
 }
 
+/*
+	Cosa fa			:			Riempie un array di caratteri con un numero n di spazi
+	sSpaces			:			char, array di caratteri da riempire con gli spazi
+	nSpaces			:			intero, numero di spazi da inserire nell'array di caratteri
+*/
 void makeSpaces(char sSpaces[],int nSpaces){
 	int i;
 	for(i=0;i<nSpaces;i++)
 		sSpaces[i] = ' ';
+
+	/* Aggiungo il carattere terminatore */
 	sSpaces[i] = '\0';
 }
 
 /*
-	Cosa fa			:			Esegue una copia di una stringa in un'altra
-	sFrom			:			stringa, stringa da copiare
-	sTo				:			stringa, stringa in cui copiare
+	Cosa fa			:			Aggiunge una stringa in coda ad un'altra
+	sWhat			:			char, array di caratteri da aggiungere in coda alla stringa
+	sTo				:			char, arrray di caratteri al quale aggiungere altro testo
 */
-void copy(char sFrom[],char sTo[]){
+void append(char sWhat[], char sTo[]){
 	int i = 0;
+	int i18;
 
-	/* Ciclo fino a quando incontro il carattere terminatore 0 */
-	while(sFrom[i]){
-		sTo[i] = sFrom[i++];
-	}
-}
+	/* Incremento la variabile i18 in modo da stabilire qual è la posizione dell'utlimo carattere */
+	for(i18 = 0;sTo[i18];i18++);
 
-void replaceChrWithString(char sBuffer[],char sRet[],int cCarattere,char sReplace[],int nStart,int nTimes){
-	int i,i2,i21,bReplaced;
-	i = i2 = bReplaced = 0;
-
-	while(sBuffer[i]){
-		if (nTimes == 0)
-			break;
-
-		if(i >= nStart){
-			if(sBuffer[i] == cCarattere){
-				for(i21 = 0;sReplace[i21] != '\0';i21++){
-					sRet[i2++] = sReplace[i21];
-					bReplaced = 1;
-				}
-				--nTimes;
-			}
-		}
-
-		if(bReplaced == 0){
-			sRet[i2] = sBuffer[i];
-			++i2;
-		}
-
-		bReplaced = 0;
-		i++;
+	/* Eseguo l'append dei caratteri */
+	while(sWhat[i]){
+		sTo[i18] = sWhat[i];
+		++i18;
+		++i;
 	}
 }
